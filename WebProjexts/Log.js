22d04e3590e1da4176362b24575409f171c7c3cd
vinisumi <131935152+vinisumi@users.noreply.github.com>
2023-05-04 12:04:04 +0530

    Add files via upload

---
+++ WebProjexts/Log.js
@@ -0,0 +1,12 @@
+
+    
+        function log(){
+            var x=Number(document.getElementById("input1").value);
+           
+            if(x<10 && x==0){
+                alert("not valid number");
+            }
+
+        }
+   
+    
